4093a9f70bc846705356b2c84d023835679e06df
Admin <taras.gunchak@kpk-lp.com.ua>
2026-04-02 14:45:39 +0300

    text message optimizing by implementing StyleSheet.create styles instead default JS object

--- app/direct_chat/components/TextMessage.tsx
+++ app/direct_chat/components/TextMessage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
 import { isCurrentUser } from "@/utils/utils";
 import { timestamp } from "../utils/utils";
 import { textStyle } from "@/styles/textStyles";
@@ -17,10 +17,10 @@ interface TextMessage_I {
 export default function TextMessage({ message }: { message: TextMessage_I }) {
   return (
     <TouchableOpacity
-      style={
+      style={[stylesR.messageView, 
         isCurrentUser(message?.sender_id)
-          ? styles.currentUser
-          : styles.notCurrentUser}>
+          ? stylesR.isCurrentUser
+          : stylesR.notCurrentUser]}>
       <Text style={[textStyle.white16]}>
         {message?.content?.message}
       </Text>
@@ -31,26 +31,25 @@ export default function TextMessage({ message }: { message: TextMessage_I }) {
   );
 };
 
-const messageView = {
-  borderColor: "rgba(255, 255, 255, 0.2)",
-  borderWidth: 0.5,
-  minHeight: 40,
-  minWidth: "10%",
-  maxWidth: "70%",
-  margin: "2%",
-  borderRadius: 8,
-  padding: 5,
-  flexDirection: "column",
-  gap: 5,
-}
-
-const styles = {
-  currentUser: [messageView, {
+const stylesR = StyleSheet.create({
+  messageView: {
+    borderColor: "rgba(255, 255, 255, 0.2)",
+    borderWidth: 0.5,
+    minHeight: 40,
+    minWidth: "10%",
+    maxWidth: "70%",
+    margin: "2%",
+    borderRadius: 8,
+    padding: 5,
+    flexDirection: "column",
+    gap: 5,
+  },
+  isCurrentUser:{
     backgroundColor: "rgba(255, 255, 255, 0.1)",
     alignSelf: "flex-end",
-  }],
-  notCurrentUser: [messageView, {
+  },
+  notCurrentUser:{
     backgroundColor: "rgba(0, 0, 0, 0.4)",
     alignSelf: "flex-start",
-  }]
-};
+  }
+});

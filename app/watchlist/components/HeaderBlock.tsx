c515ea82753e457458fb9ddfdf33361805232a48
tarashunchak <taras.gunchak@kpk-lp.com.ua>
2026-05-04 22:37:04 +0300

    HeaderBlock.tsx in WatchlistDetailsScreen refactoring

--- app/watchlist/components/HeaderBlock.tsx
+++ app/watchlist/components/HeaderBlock.tsx
@@ -1,20 +1,30 @@
 import React from "react";
-import { Text, Image, View, TouchableOpacity } from "react-native";
+import { Text, View, TouchableOpacity, StyleSheet } from "react-native";
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from "react-native-responsive-screen"
 import { textStyle } from "@/styles/textStyles";
 import { useNavigation } from "expo-router";
+import { Image } from "expo-image";
 
 export default function HeaderBlock({ watchlist }: { watchlist: any }) {
   const navigator = useNavigation();
   return (
-    <View style={{ justifyContent: "flex-end", height: hp("41%") - 50 }}>
-      <View style={[{ flexDirection: "row", justifyContent: "space-between" }]}>
-        <View style={[styles.info.view]}>
-          <Image source={watchlist?.fg_img_url ? { uri: watchlist?.fg_img_url } : require("@/app/library/assets/NoFgWatchlist.png")} style={styles.info.image} />
+    <View style={styles.container}>
+      <View style={styles.mainView}>
+        <View style={[styles.infoView]}>
+          <Image
+            source={
+              watchlist?.fg_img_url ? { uri: watchlist?.fg_img_url }
+                : require("@/app/library/assets/NoFgWatchlist.png")}
+            style={styles.image}
+            cachePolicy="memory"
+          />
 
-          <View style={styles.card.text.view}>
-            <Text style={styles.card.text.name}>{watchlist?.name}</Text>
-            <Text style={styles.card.text.description}
+          <View style={styles.textView}>
+            <Text style={[styles.nameText, textStyle.yellow22]}>
+              {watchlist?.name}
+            </Text>
+            <Text
+              style={[styles.descriptionText, textStyle.gray16]}
               pointerEvents="none"
               numberOfLines={2}
               ellipsizeMode="tail"
@@ -22,10 +32,12 @@ export default function HeaderBlock({ watchlist }: { watchlist: any }) {
               {watchlist?.description}
             </Text>
             <TouchableOpacity
-              style={styles.card.text.creator.view}
+              style={styles.creatorTileView}
               onPress={() => navigator.navigate("UserProfileScreen", { userID: watchlist?.creator_id })}>
-              <Text style={styles.card.text.creator.header}>Creator:</Text>
-              <Text style={styles.card.text.creator.name}>{watchlist?.creator_username}</Text>
+              <Text style={textStyle.gray14}>Creator:</Text>
+              <Text style={textStyle.yellow14}>
+                {watchlist?.creator_username}
+              </Text>
             </TouchableOpacity>
           </View>
         </View>
@@ -37,75 +49,54 @@ export default function HeaderBlock({ watchlist }: { watchlist: any }) {
   )
 }
 
-const styles = {
-  info: {
-    view: {
-      height: hp("15%"),
-      width: wp("100%"),
-      backgroundColor: "rgba(255,255, 255, 0.05)",
-      borderColor: "rgba(255,255, 255, 0.2)",
-      borderWidth: 0.8,
-      borderTopLeftRadius: 8,
-      borderTopRightRadius: 8,
-      padding: hp("1%"),
-      gap: hp("2%"),
-      flexDirection: "row",
-      alignItems: "flex-start",
-      justifyContent: "flex-start",
-    },
-    image: {
-      width: hp("13%"),
-      aspectRatio: 1,
-      borderRadius: 4,
-    }
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: "flex-end",
+    height: hp("41%") - 50
   },
-  card: {
-    view: {
-      gap: 10,
-      height: hp("15%"),
-      backgroundColor: "rgba(255, 255, 255, 0.03)",
-      borderRadius: 6,
-      borderWidth: 1,
-      borderColor: "rgba(255, 255, 255, 0.05)",
-      padding: hp("0.5%"),
-      flexDirection: "row",
-    },
-    image: {
-      height: "100%",
-      width: "30%",
-      resizeMode: "cover",
-      borderRadius: 4,
-    },
-    text: {
-      view: {
-        flexDirection: "column",
-        gap: 2,
-      },
-      name: [textStyle.yellow22, {
-
-      }],
-      description: [textStyle.gray16, {
-        maxWidth: "75%",
-        minWidth: "75%",
-      }],
-      creator: {
-        view: {
-          flexDirection: "row",
-          gap: 5,
-          borderWidth: 0.5,
-          borderColor: "rgba(255, 255, 255, 0.2)",
-          borderRadius: 3,
-          backgroundColor: "rgba(255, 255, 255, 0.05)",
-          padding: 2,
-          alignSelf: "flex-start",
-        },
-        header: [textStyle.gray14, {
-
-        }],
-        name: [textStyle.yellow14, {
-
-        }],
-      }
-    }
+  mainView: {
+    flexDirection: "row",
+    justifyContent: "space-between"
+  },
+  creatorTileView: {
+    flexDirection: "row",
+    gap: 5,
+    borderWidth: 0.5,
+    borderColor: "rgba(255, 255, 255, 0.2)",
+    borderRadius: 3,
+    backgroundColor: "rgba(255, 255, 255, 0.05)",
+    padding: 2,
+    alignSelf: "flex-start",
+  },
+  descriptionText: {
+    maxWidth: "75%",
+    minWidth: "75%",
+  },
+  nameText: {
+    maxWidth: "75%",
+    minWidth: "75%",
+  },
+  image: {
+    width: hp("13%"),
+    aspectRatio: 1,
+    borderRadius: 4,
+  },
+  infoView: {
+    height: hp("15%"),
+    width: wp("100%"),
+    backgroundColor: "rgba(255,255, 255, 0.05)",
+    borderColor: "rgba(255,255, 255, 0.2)",
+    borderWidth: 0.8,
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+    padding: hp("1%"),
+    gap: hp("2%"),
+    flexDirection: "row",
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+  },
+  textView: {
+    flexDirection: "column",
+    justifyContent: "space-between",
   }
-} 
+}); 
